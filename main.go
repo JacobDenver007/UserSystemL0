c2efa85a8cbd2ba8c5d5b6b47e075e5a01c9160a
jacobdenver007 <jacobdenver007@gmail.com>
2018-09-07 18:26:29 +0800

    fix

--- main.go
+++ main.go
@@ -12,15 +12,13 @@ func main() {
 	listenport := flag.Int("listenport", 9000, "api listen port")
 
 	go func() {
-		router := gin.Default()
-		router.Use(user.Checklogged())
-		user.RegisterAPI(router)
-		if err := router.Run(fmt.Sprintf(":%d", *listenport)); err != nil {
-			panic(err)
-		}
-	}()
-
-	go func() {
 		user.Init()
 	}()
+
+	router := gin.Default()
+	router.Use(user.Checklogged())
+	user.RegisterAPI(router)
+	if err := router.Run(fmt.Sprintf(":%d", *listenport)); err != nil {
+		panic(err)
+	}
 }
